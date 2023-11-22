63385c2201f32cee0c21797454266331b2b4b31f
bhaskardas090 <bhaskardas090@gmail.com>
2023-11-22 10:14:03 +0530

    added the play and more info button in heroVideo

--- src/common/HeroVideo.js
+++ src/common/HeroVideo.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./heroVideo.scss";
+import { FaPlay } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 
 const HeroVideo = () => {
   return (
     <div className="heroVideo">
       <img
         className="video"
-        src="https://wallpapercave.com/wp/wp6889318.jpg"
+        src="https://wallpapers.com/images/featured/squid-game-fvsfw2qlkey7u5o8.jpg"
         alt="hero-video"
       />
+      <div className="heroVideoButtons">
+        <button className="play">
+          <FaPlay className="playIcon" />
+          Play
+        </button>
+        <button className="info">
+          <IoIosInformationCircleOutline className="infoIcon" />
+          More Info
+        </button>
+      </div>
     </div>
   );
 };
